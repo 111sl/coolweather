c6e0671bdd7cf19a3fc7257b15342c03531b1398
zhengzong <zhengzong@qq.com>
2019-12-03 16:10:48 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/uncledrew/coolweather/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.example.uncledrew.coolweather.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String api;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/example/uncledrew/coolweather/util/Utility.java
+++ app/src/main/java/com/example/uncledrew/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.uncledrew.coolweather.db.City;
 import com.example.uncledrew.coolweather.db.County;
 import com.example.uncledrew.coolweather.db.Province;
+import com.example.uncledrew.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -87,4 +89,20 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的json数据解析成weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
